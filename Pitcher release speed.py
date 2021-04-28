b6eb831d72dd5c0bec99d970d7624f666d3338fc
arete89 <75988771+arete89@users.noreply.github.com>
2021-04-29 00:27:04 +0900

    Add files via upload

---
+++ Pitcher release speed.py
@@ -0,0 +1,10 @@
+from pybaseball import playerid_lookup
+from pybaseball import statcast_pitcher
+
+playerid_lookup('ryu', 'hyun jin').key_mlbam
+# Always use lowercase & last name goes first and then first name.
+# His player ID is 547943. Let's make a DF based on this.
+ryu_stats = statcast_pitcher('2019-04-01', '2019-09-01', 547943)
+ryu_stats.head()
+# Let's find out his fastest and slowest pitch speed.
+ryu_stats.release_speed.sort_values()
